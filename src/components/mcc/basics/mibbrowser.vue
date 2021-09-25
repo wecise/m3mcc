cfafbe6ace8f32b498761e8094c630af3ae87299
libf <libf@wecise.com>
2021-09-25 20:30:54 +0800

    .

--- src/components/mcc/basics/mibbrowser.vue
+++ src/components/mcc/basics/mibbrowser.vue
@@ -3,62 +3,92 @@
         <el-header>
             <span style="font-weight:900;">MIB Browser</span>
         </el-header>
-        <el-main>
-            <div style="height: 40px;padding: 10px 0 0 0;border-bottom:1px solid #dddddd;">
-                <el-form :inline="true">
-                    <el-form-item label="地址" label-width="40px">
-                        <el-input  placeholder="127.0.0.1" style="width:100px;"></el-input>
-                    </el-form-item>
-                    <el-form-item label="端口" label-width="40px">
-                        <el-input  placeholder="161" style="width:60px;"></el-input>
-                    </el-form-item>
-                    <el-form-item label="Community" label-width="70px">
-                        <el-input placeholder="********" style="width:100px;"></el-input>
-                    </el-form-item>
-                    <el-form-item label="版本" label-width="40px">
-                        <el-select value="1" placeholder="V1" style="width:60px;">
-                            <el-option label="V1" value="1"></el-option>
-                            <el-option label="V2" value="2"></el-option>
-                            <el-option label="V3" value="3"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="text" icon="el-icon-setting"></el-button>
-                    </el-form-item>
-                    <el-form-item label="OID" label-width="40px">
-                        <el-input placeholder=".1.3.6.1.4.1.2021.10.1.5.1" style="width:180px;"></el-input>
-                    </el-form-item>
-                    <el-form-item label="操作" label-width="40px">
-                        <el-select value="GetSubtree" placeholder="GetSubtree" style="width:120px;">
-                            <el-option label="Get" value="Get"></el-option>
-                            <el-option label="GetSubtree" value="GetSubtree"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary">执行</el-button>
-                    </el-form-item>
-                </el-form>
+        <el-main style="overflow:hidden; display:flex; flex-flow:column nowrap;">
+            <div style="background:#ffffff; overflow:hidden; flex:0 0 auto;">
+                <div style="height: 40px;padding: 10px 0 0 0;border-bottom:1px solid #dddddd;">
+                    <el-form :inline="true">
+                        <el-form-item label="地址" label-width="40px">
+                            <el-input v-model="snmp_o_p.ip" placeholder="127.0.0.1" style="width:100px;"></el-input>
+                        </el-form-item>
+                        <el-form-item label="端口" label-width="40px">
+                            <el-input v-model="snmp_o_p.port" placeholder="161" style="width:60px;"></el-input>
+                        </el-form-item>
+                        <el-form-item label="Community" label-width="70px">
+                            <el-input v-model="snmp_o_p.community" placeholder="********" style="width:100px;"></el-input>
+                        </el-form-item>
+                        <el-form-item label="版本" label-width="40px">
+                            <el-select v-model="snmp_o_p.version" placeholder="1" style="width:70px;">
+                                <el-option label="V1" value="1"></el-option>
+                                <el-option label="V2" value="2"></el-option>
+                                <el-option label="V3" value="3"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="text" @click="run" icon="el-icon-setting"></el-button>
+                        </el-form-item>
+                        <el-form-item label="OID" label-width="40px">
+                            <el-input v-model="snmp_o_p.OID" placeholder=".1.3.6.1.4.1.2021.10.1.5.1" style="width:180px;"></el-input>
+                        </el-form-item>
+                        <el-form-item label="操作" label-width="40px">
+                            <el-select v-model="snmp_o_p.op" placeholder="Get" style="width:120px;">
+                                <el-option label="Get" value="Get"></el-option>
+                                <el-option label="GetSubtree" value="GetSubtree"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary" @click="run">执行</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
             </div>
-            <Split style="background:#ffffff;">
-                <SplitArea :size="20" :minSize="0" style="overflow:hidden;">
-                    <mibtree></mibtree>
-                </SplitArea>
-                <SplitArea :size="80" :minSize="0" style="overflow:hidden;">
-                    <div style="font-weight:900;height:30px;line-height:30px;padding:0 10px;">SNMP Mibs</div>
-                    
-                </SplitArea>
-            </Split>
+            <div style="overflow:hidden; flex: 1 1 auto;">
+                <Split style="background:#ffffff;">
+                    <SplitArea :size="25" :minSize="0" style="overflow:hidden;">
+                        <mibtree @node-click="onNodeClick"></mibtree>
+                    </SplitArea>
+                    <SplitArea :size="75" :minSize="0" style="overflow:auto;">
+                        <el-header>
+                            Result Table
+                        </el-header>
+                        <el-table
+                            class="result"
+                            :data="result"
+                            border
+                            style="display:flex; flex-flow:column nowrap;">
+                            <el-table-column v-for="rc in result_columns" :key="rc.prop" :prop="rc.prop" :label="rc.label">
+                            </el-table-column>
+                        </el-table>
+                    </SplitArea>
+                </Split>
+            </div>
         </el-main>
     </el-container>
 </template>
 <script>
     import _ from 'lodash';
+    import $ from 'jquery';
     import mibtree from './mibtree.vue';
 
     export default {
         components: { mibtree },
         data(){
             return {
+                snmp_o_p: {
+                    ip: "127.0.0.1",
+                    port: 161,
+                    community: "haoba_public",
+                    version: "1",
+                    OID: ".1.3.6.1.4.1.2021.10.1.5.1",
+                    op: "Get",
+                },
+                result: [],
+                result_columns: [
+                    {prop:"Time", label:"Time"},
+                    {prop:"Addr", label:"IP:Port"},
+                    {prop:"Key", label:"Name/OID"},
+                    {prop:"Value", label:"Value"},
+                    {prop:"Type", label:"Type"},
+                ],
                 tree:{
                     data: [
                         {
@@ -107,7 +137,54 @@
         },
         methods:{
             onNodeClick(node){
-                
+                if(node.Kind == "Table"){
+                    this.snmp_o_p.OID = node.OID
+                } else {
+                    this.snmp_o_p.OID = node.OID+".0"
+                }
+            },
+            run(){
+                let mdata = this.$data
+                // $.ajax({
+                //     url: "http://127.0.0.1:10801/get?op="+this.snmp_o_p.op+"&OID="+this.snmp_o_p.OID+"&ip="+this.snmp_o_p.ip+"&port="+this.snmp_o_p.port+"&community="+this.snmp_o_p.community+"&version="+this.snmp_o_p.version,
+                //     dataType: 'jsonp',
+                //     type: 'GET',
+                //     success(data) {
+                //         //console.debug("onNodeClick",JSON.stringify(data))
+                //         if (data && data.Result) {
+                //             for (var i=0; i<data.Result.length; i++) {
+                //                 mdata.result.push(data.Result[i])
+                //             }
+                //         }
+                //     },
+                //     error(err){
+                //         console.error("getMIBTree", JSON.stringify(err))
+                //     }
+                // });
+                this.m3
+                    .callService("m3service.mibbrowser", "get", {
+                        op: this.snmp_o_p.op,
+                        OID: this.snmp_o_p.OID,
+                        ip: this.snmp_o_p.ip,
+                        port: this.snmp_o_p.port,
+                        community: this.snmp_o_p.community,
+                        version: this.snmp_o_p.version,
+                    })
+                    .then((res) => {
+                        let data = res.message;
+                        if(data.Error) {
+                            console.error("", data.Error);
+                        } else {
+                            data = data.Result;
+                            //console.debug("onNodeClick",JSON.stringify(data))
+                            mdata.result = []
+                            if (data && data.Result) {
+                                for (var i=0; i<data.Result.length; i++) {
+                                    mdata.result.push(data.Result[i])
+                                }
+                            }
+                        }
+                    });
             }
         }
     }
--- src/components/mcc/basics/mibtree.vue
+++ src/components/mcc/basics/mibtree.vue
@@ -1,165 +1,230 @@
 <template>
-    <el-container>
-        <el-header>
-            SNMP MIBS
-        </el-header>
-        <el-main>
-            <Split style="background:#ffffff;" direction="vertical">
-                <SplitArea :size="60" :minSize="0" style="overflow:hidden;">
-                    <el-tree :data="tree.data" :props="tree.defaultProps"
-                        @node-click="onNodeClick"></el-tree>
-                </SplitArea>
-                <SplitArea :size="40" :minSize="0" style="overflow:hidden;">
-                    <el-table
-                        :data="dt.rows"
-                        height="120"
-                        border
-                        style="width: 100%">
-                        <el-table-column
-                        prop="key"
-                        label="Key"
-                        width="150">
-                        </el-table-column>
-                        <el-table-column
-                        prop="value"
-                        label="Value"
-                        width="150">
-                        </el-table-column>
-                    </el-table>
-                </SplitArea>
-            </Split>
-        </el-main>
-    </el-container>
+  <div
+    style="
+      overflow: hidden;
+      height: 100%;
+      display: flex;
+      flex-flow: column nowrap;
+    "
+  >
+    <el-header> SNMP MIBS </el-header>
+    <div style="overflow: hidden; flex: 1 1 100%">
+      <Split style="background: #ffffff" direction="vertical">
+        <SplitArea
+          :size="60"
+          :minSize="1"
+          style="overflow: auto; overflow-x: auto; overflow-y: auto"
+        >
+          <el-tree
+            :data="tree.data"
+            :props="tree.defaultProps"
+            @node-click="onNodeClick"
+          ></el-tree>
+        </SplitArea>
+        <SplitArea
+          :size="40"
+          :minSize="1"
+          style="overflow: auto; overflow-x: auto; overflow-y: auto"
+        >
+          <el-table
+            class="mib-info"
+            :show-header="false"
+            :data="dt.rows"
+            border
+            style="display: flex; flex-flow: column nowrap"
+          >
+            <el-table-column prop="key" label="Key" style="width: 50%">
+            </el-table-column>
+            <el-table-column prop="value" label="Value" style="width: 50%">
+            </el-table-column>
+          </el-table>
+        </SplitArea>
+      </Split>
+    </div>
+  </div>
 </template>
 <script>
-    import _ from 'lodash';
-    import $ from 'jquery'
+import _ from "lodash";
+import $ from "jquery";
 
-    export default {
-        data(){
-            return {
-                tree:{
-                    data: [
-                        {
-                            Name: 'MIB Tree',
-                            Children: [
-                                {
-                                    Name: 'iso.org.dod.internet',
-                                    Children: [
-                                        {
-                                            Name: 'mgmt',
-                                            Children: [
-                                                {
-                                                    Name: '...',
-                                                    Children: []
-                                                }
-                                            ]
-                                        }, 
-                                        {
-                                            Name: 'snmpV2',
-                                            Children: [
-                                                {
-                                                    Name: '...',
-                                                    Children: []
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
+export default {
+  data() {
+    return {
+      tree: {
+        data: [
+          {
+            Name: "MIB Tree",
+            Children: [
+              {
+                Name: "iso.org.dod.internet",
+                Children: [
+                  {
+                    Name: "mgmt",
+                    Children: [
+                      {
+                        Name: "...",
+                        Children: [],
+                      },
                     ],
-                    defaultProps: {
-                        children: 'Children',
-                        label: 'Name'
-                    }
-                },
-                dt:{
-                    rows: [],
-                    columns: []
-                }
-            }
+                  },
+                  {
+                    Name: "snmpV2",
+                    Children: [
+                      {
+                        Name: "...",
+                        Children: [],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        defaultProps: {
+          children: "Children",
+          label: "Name",
         },
-        mounted(){
-            this.getMIBTree();
-        },
-        methods:{
-            onNodeClick(node){
-                //console.debug("onNodeClick",JSON.stringify(node))
-                let dtrows = []
-                for (var k in node) {
-                    //console.debug("type of", k, typeof(node[k]))
-                    if (typeof(node[k]) in {"string":1,"number":1}){
-                        dtrows.push({
-                            key: k,
-                            value: node[k]
-                        });
-                    }
-                }
-                this.$data.dt.rows = dtrows
-                $.ajax({
-                    url: "http://127.0.0.1:10801/mibtree?n="+node.Name+"&x=2",
-                    dataType: 'jsonp',
-                    type: 'GET',
-                    success(data) {
-                        //console.debug("onNodeClick",JSON.stringify(data))
-                        if (data && data.MIBTree && data.MIBTree[0] && data.MIBTree[0].Children) {
-                            node.Children = data.MIBTree[0].Children
-                        }
-                    },
-                    error(err){
-                        console.error("getMIBTree", JSON.stringify(err))
-                    }
-                });
-            },
-            getMIBTree(){
-                let mdata = this.$data
-                let param = encodeURIComponent( JSON.stringify({}) );
-                $.ajax({
-                    url: "http://127.0.0.1:10801/mibtree?n=internet&x=2",
-                    dataType: 'jsonp',
-                    type: 'GET',
-                    success(data) {
-                        //console.debug("getMIBTree",JSON.stringify(data))
-                        data.MIBTree[0].Name = "iso.org.dod.internet"
-                        data.MIBTree[0].Children = data.MIBTree[0].Children.filter(function(item) {
-                            return item.Name in {"mgmt":1,"snmpV2":1};
-                        });
-                        mdata.tree.data = data.MIBTree
-                    },
-                    error(err){
-                        console.error("getMIBTree", JSON.stringify(err))
-                    }
-                });
-                // this.m3.callFS("/matrix/m3mcc/mibtree.js", param).then(res=>{
-                //     this.tree = res.message.MIBTree
-                // })
-            }
+      },
+      dt: {
+        rows: [],
+        columns: [],
+      },
+    };
+  },
+  mounted() {
+    this.getMIBTree();
+  },
+  methods: {
+    onNodeClick(node) {
+      //console.debug("onNodeClick",JSON.stringify(node))
+      let dtrows = [];
+      for (var k in node) {
+        //console.debug("type of", k, typeof(node[k]))
+        if (typeof node[k] in { string: 1, number: 1 }) {
+          dtrows.push({
+            key: k,
+            value: node[k],
+          });
         }
-    }
+      }
+      this.$data.dt.rows = dtrows;
+    //   $.ajax({
+    //     url: "http://127.0.0.1:10801/mibtree?n=" + node.Name + "&x=2",
+    //     dataType: "jsonp",
+    //     type: "GET",
+    //     success(data) {
+    //       //console.debug("onNodeClick",JSON.stringify(data))
+    //       if (
+    //         data &&
+    //         data.MIBTree &&
+    //         data.MIBTree[0] &&
+    //         data.MIBTree[0].Children
+    //       ) {
+    //         node.Children = data.MIBTree[0].Children;
+    //       }
+    //     },
+    //     error(err) {
+    //       console.error("getMIBTree", JSON.stringify(err));
+    //     },
+    //   });
+      this.m3
+        .callService("m3service.mibbrowser", "mibtree", {
+          n: node.Name,
+          x: 2,
+        })
+        .then((res) => {
+          let data = res.message;
+          if(data.Error) {
+            console.error("", data.Error);
+          } else {
+            data = data.Result;
+            //console.debug("onNodeClick",JSON.stringify(data))
+            if (
+                data &&
+                data.MIBTree &&
+                data.MIBTree[0] &&
+                data.MIBTree[0].Children
+            ) {
+                node.Children = data.MIBTree[0].Children;
+            }
+          }
+        });
+      this.$emit("node-click", node);
+    },
+    getMIBTree() {
+      let mdata = this.$data;
+    //   $.ajax({
+    //     url: "http://127.0.0.1:10801/mibtree?n=internet&x=2",
+    //     dataType: "jsonp",
+    //     type: "GET",
+    //     success(data) {
+    //       //console.debug("getMIBTree",JSON.stringify(data))
+    //       data.MIBTree[0].Name = "iso.org.dod.internet";
+    //       data.MIBTree[0].Children = data.MIBTree[0].Children.filter(function (
+    //         item
+    //       ) {
+    //         return item.Name in { mgmt: 1, snmpV2: 1 };
+    //       });
+    //       mdata.tree.data = data.MIBTree;
+    //     },
+    //     error(err) {
+    //       console.error("getMIBTree", JSON.stringify(err));
+    //     },
+    //   });
+      this.m3
+        .callService("m3service.mibbrowser", "mibtree", {
+          n: "internet",
+          x: 2,
+        })
+        .then((res) => {
+          let data = res.message;
+          if(data.Error) {
+              console.error("", data.Error);
+          } else {
+            data = data.Result;
+            //console.debug("getMIBTree",JSON.stringify(data))
+            data.MIBTree[0].Name = "iso.org.dod.internet";
+            data.MIBTree[0].Children = data.MIBTree[0].Children.filter(function (
+                item
+            ) {
+                return item.Name in { mgmt: 1, snmpV2: 1 };
+            });
+            mdata.tree.data = data.MIBTree;
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .el-container{
-        height: calc(100vh - 80px);
-    }
-    .el-header{
-        height: 40px!important;
-        line-height: 40px;
-        padding: 0px 10px;
-        background: #f2f2f2;
-    }
-    .el-main{
-        padding: 0px;
-        overflow: hidden;
-        background: #ffffff;
-    }
+.el-container {
+  height: calc(100vh - 80px);
+}
+.el-header {
+  height: 40px !important;
+  line-height: 40px;
+  padding: 0px 10px;
+  background: #f2f2f2;
+}
+.el-main {
+  padding: 0px;
+  overflow: hidden;
+  background: #ffffff;
+}
 </style>
 <style>
-    .el-tree{
-        height: 100%;
-    }
-    .el-tree-node__label {
-        font-size: 12px!important;
-    }
+.el-tree {
+  height: 100%;
+}
+.el-tree-node__label {
+  font-size: 12px !important;
+}
+.mib-info,
+.mib-info .el-table__body-wrapper {
+  overflow: visible !important;
+  overflow-x: visible !important;
+  overflow-y: visible !important;
+}
 </style>
